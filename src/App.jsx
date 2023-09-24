67eef25ab37e5870fa023b90bf16e62cf820bff5
Mankaran7 <mankaransingh22747@gmail.com>
2023-09-24 23:49:25 +0530

    added react hot toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import toast from 'react-hot-toast'
 
 function App() {
   
-
+  useEffect(()=>{
+    toast.success("hello")
+  })
   return (
+
     <>
       <h1 className="text-3xl font-bold underline">
       Hello world!
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 )

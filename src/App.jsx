068cba29e0aaafb0724f181f068cebd87082a120
Mankaran7 <mankaransingh22747@gmail.com>
2023-09-25 21:13:05 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-
-import { useEffect } from 'react'
 import './App.css'
-import toast from 'react-hot-toast'
 
 function App() {
-  
-  useEffect(()=>{
-    toast.success("hello")
-  })
-  return (
 
-    <>
-      <h1 className="text-3xl font-bold underline">
+  return (
+    <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    </>
   )
 }
 

06be53643c6319f84d1d2c8d906bc4d94acd643d
Mankaran7 <mankaransingh22747@gmail.com>
2023-09-28 21:46:20 +0530

    added sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-
+import Signin from './pages/Signin'
 import Aboutus from './pages/AboutUs'
 import Home from './pages/Home'
 import Notfound from './pages/Notfound'
@@ -12,6 +12,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<Aboutus />} />
       <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<Signin />} />
       <Route path='*' element={<Notfound />} />
     </Routes>
   )
